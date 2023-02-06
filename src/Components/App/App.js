05e590fc4e672cbbef69e5583226b46e9302b937
Evan Swanson <evan.sundelius@gmail.com>
2023-02-06 14:24:00 -0600

    Set up redux

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes, NavLink } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
