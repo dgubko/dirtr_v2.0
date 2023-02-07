89b1ea7efd9cc2ca70f3b730d100ce850b6903c8
Dana Truong <tramtramphotos@gmail.com>
2023-02-07 14:02:42 -0600

    Add misc. details and overview

--- src/Components/IndividualTrail/IndividualTrail.js
+++ src/Components/IndividualTrail/IndividualTrail.js
@@ -8,8 +8,18 @@ function IndividualTrail() {
       <div className='individual-trail-container'>
         <div className='trail-details-container'>
           <div className='trail-name-button-styling'>
-            <h2>Very Scenic Trail</h2>
-            <button>❤️</button>
+            <h2 className='trail-name'>Very Scenic Trail</h2>
+            <button className='trail-button'>❤️</button>
+          </div>
+          <div className='trail-addl-detail-styling'>
+            <p className='trail-distance'>Distance: 12 miles</p>
+            <p className='trail-duration'>Duration: 6 hours</p>
+            <p className='trail-elevation'>Elevation: 3 miles</p>
+            <p className='trail-difficulty'>Difficulty: Shmedium</p>
+          </div>
+          <div className='trail-overview'>
+            <h2 className='trail-overview-title'>Overview</h2>
+            <p className='trail-overview-description'>Kinda lengthy, but not too lengthy. Kinda easy, but not too easy. Ramble ramble ramble.</p>
           </div>
         </div>
         <div className='trail-map-container'>
