948f45bcf081652412be26fa887e404e5efe654b
Dana Truong <tramtramphotos@gmail.com>
2023-02-07 12:31:13 -0600

    Create IndividualTrail Component

---
+++ src/Components/IndividualTrail/IndividualTrail.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './IndividualTrail.css'
+
+function IndividualTrail() {
+  return (
+    <div>
+      <h2>Individual Trail</h2>
+    </div>
+  )
+}
+
+export default IndividualTrail
