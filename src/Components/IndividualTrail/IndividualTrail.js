5b0f5be31093df0b33a47eaa6534f725d02882a4
Dana Truong <tramtramphotos@gmail.com>
2023-02-07 12:55:51 -0600

    Add trail map placeholder

--- src/Components/IndividualTrail/IndividualTrail.js
+++ src/Components/IndividualTrail/IndividualTrail.js
@@ -3,8 +3,19 @@ import './IndividualTrail.css'
 
 function IndividualTrail() {
   return (
-    <div>
-      <h2>Individual Trail</h2>
+    <div className='individual-trail'>
+      <h2>Individual Trail View</h2>
+      <div className='individual-trail-container'>
+        <div className='trail-details-container'>
+          <div className='trail-name-button-styling'>
+            <h2>Very Scenic Trail</h2>
+            <button>❤️</button>
+          </div>
+        </div>
+        <div className='trail-map-container'>
+          <img width='500' src='https://i0.wp.com/www.naturetrailsnc.com/wp-content/uploads/Screen_Shot_2019-02-11_at_11.10.23_PM-bb.jpg?ssl=1' />
+        </div>
+      </div>
     </div>
   )
 }
