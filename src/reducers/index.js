79830a4ef9aeafbf1643a39d79e02778809374d3
Dmitrii Gubko <dalnoboy29region@gmail.com>
2023-02-13 16:10:49 -0700

    Add trails and counties to store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,14 @@
-import counterReducer from './counter'
-import loggedReducer from './isLogged'
-import { combineReducers } from 'redux'
+import counterReducer from "./counter";
+import loggedReducer from "./isLogged";
+import { combineReducers } from "redux";
+import { trailsReducer } from "./trails";
+import { countyReducer } from "./counties";
 
 const rootReducer = combineReducers({
-    counter: counterReducer,
-    isLogged: loggedReducer
-})
+  counter: counterReducer,
+  isLogged: loggedReducer,
+  trails: trailsReducer,
+  counties: countyReducer,
+});
 
-export default rootReducer
+export default rootReducer;
