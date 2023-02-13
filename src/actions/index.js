60180afc9a10e35f8ae97cc965ea3b9e0ba94fe2
Dmitrii Gubko <dalnoboy29region@gmail.com>
2023-02-13 16:11:56 -0700

    Add action to save all counties

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,22 +1,27 @@
-export const increment = nr => {
-    return{
-        type: 'INCREMENT',
-        payload: nr
-    };
+export const increment = (nr) => {
+  return {
+    type: "INCREMENT",
+    payload: nr,
+  };
 };
-export const decrement = nr => {
-    return{
-        type: 'DECREMENT',
-        payload: nr
-    };
+export const decrement = (nr) => {
+  return {
+    type: "DECREMENT",
+    payload: nr,
+  };
 };
 export const logToggle = () => {
-    return{
-        type: 'SIGN_IN'
-    }
+  return {
+    type: "SIGN_IN",
+  };
 };
 
 export const saveAllTrails = (trails) => ({
   type: "SAVE_ALL_TRAILS",
   payload: trails,
+});
+
+export const saveAllCounties = (counties) => ({
+  type: "SAVE_ALL_COUNTIES",
+  payload: counties,
 });
