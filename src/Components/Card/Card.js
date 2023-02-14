bb6db4311026237e3f0c374362fd68c0af0e0dbb
Dmitrii Gubko <dalnoboy29region@gmail.com>
2023-02-14 15:33:29 -0700

    Display all user trails on the page

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -2,7 +2,7 @@ import React, { useReducer } from "react";
 import "./Card.css";
 import { NavLink } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { setTrail } from "../../actions"
+import { setTrail, saveUserTrail } from "../../actions";
 import { postToFavorites } from "../../utilis/apiCalls";
 
 function Card(props) {
@@ -11,21 +11,23 @@ function Card(props) {
       return county.id === props.countyId.toFixed();
     })
   );
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const selectTrail = (id) => {
-    return fetch('http://localhost:3000/api/v1/trail?id=' + id, {
-    })
-      .then(response => response.json())
-      .then(data => dispatch(setTrail(data)))
-      .catch(err => console.log(err))
+    return fetch("http://localhost:3000/api/v1/trail?id=" + id, {})
+      .then((response) => response.json())
+      .then((data) => dispatch(setTrail(data)))
+      .catch((err) => console.log(err));
+  };
   const currentUser = useSelector((state) => state.selectedUser);
   const trails = useSelector((state) => state.trails);
 
   const addToFavorites = (propsId) => {
-    const foundTrail = trails.find(trail => trail.id === propsId)
-    postToFavorites(foundTrail, currentUser.data.id)
-  }
+    const foundTrail = trails.find((trail) => trail.id === propsId);
+    postToFavorites(foundTrail, currentUser.data.id).then(() => {
+      dispatch(saveUserTrail(foundTrail));
+    });
+  };
 
   return (
     <div className="card">
@@ -39,14 +41,17 @@ function Card(props) {
           <div className="trail-county-container">
             <p className="trail-name">{props.name}</p>
             <div className="divider-bar"></div>
-            <p className="county-name">{county.name}</p>
+            <p className="county-name">{county ? county.name : "Unknown"}</p>
           </div>
         </div>
       </NavLink>
       <div className="card-bottom">
         <p className="trail-difficulty">{props.difficulty}</p>
         <p className="trail-distance">{props.distance} miles</p>
-        <button className="favorite-button" onClick={() => addToFavorites(props.id)}>
+        <button
+          className="favorite-button"
+          onClick={() => addToFavorites(props.id)}
+        >
           <div className="heart-image-container" alt="favorite button" />
         </button>
       </div>
--- src/Components/SavedTrails/SavedTrails.js
+++ src/Components/SavedTrails/SavedTrails.js
@@ -1,18 +1,43 @@
-import React from 'react'
-import './SavedTrails.css'
-import Card from '../Card/Card'
+import React, { useEffect } from "react";
+import "./SavedTrails.css";
+import Card from "../Card/Card";
+import { getUserSavedTrails } from "../../apiCalls/getUserSavedTrails";
+import { useDispatch } from "react-redux";
+import { saveAllUserTrails } from "../../actions";
+import { useSelector } from "react-redux";
 
 function SavedTrails() {
+  const savedTrails = useSelector((state) => state.savedTrails);
+  const dispatch = useDispatch();
+  const id = useSelector((state) => state.selectedUser.data.id);
+  console.log(id);
+
+  useEffect(() => {
+    getUserSavedTrails(id).then(({ data }) => {
+      dispatch(saveAllUserTrails(data.attributes.trails));
+    });
+  }, []);
+
   return (
-    <div className='saved-trails'>
-      <h2 className='saved-trails-title'>Saved Trails View</h2>
-      <div className='saved-trails-card-container'>
-        <Card />
-        <Card />
-        <Card />
+    <div className="saved-trails">
+      <h2 className="saved-trails-title">Saved Trails View</h2>
+      <div className="saved-trails-card-container">
+        {savedTrails.map((trail) => {
+          return (
+            <Card
+              key={trail.id}
+              id={trail.id}
+              name={trail.name}
+              countyId={trail.county_id}
+              distance={trail.distance}
+              difficulty={trail.difficulty}
+              image={trail.thumbnail_image}
+            />
+          );
+        })}
       </div>
     </div>
-  )
+  );
 }
 
-export default SavedTrails
+export default SavedTrails;
