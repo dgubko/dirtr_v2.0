584680cf7ce12d132ff7122213323a3cc76cf61e
Dana Truong <tramtramphotos@gmail.com>
2023-02-15 15:06:19 -0600

    Test welcome page user flow

--- cypress/e2e/welcome_spec.cy.js
+++ cypress/e2e/welcome_spec.cy.js
@@ -1,7 +1,18 @@
 describe('Welcome Page User Flow', () => {
   beforeEach(() => {
+    cy.intercept(
+      'https://murmuring-cliffs-65490.herokuapp.com/api/v1/trails',
+      {
+        method: 'GET',
+        fixture: '../fixtures/alltrails.json',
+      }
+    )
+    cy.intercept('https://murmuring-cliffs-65490.herokuapp.com/api/v1/user?id=3')
+    cy.intercept('https://murmuring-cliffs-65490.herokuapp.com/api/v1/counties')
+    cy.intercept('https://murmuring-cliffs-65490.herokuapp.com/api/v1/users')
     cy.visit('http://localhost:3001/')
-    // we need to expand this to click a user button and click log in so it starts on the home page
+    cy.get('.user-button').eq(2).click()
+    cy.get('.enter-button').click()
   })
 
   it('Should see Dirtr title', () => {
@@ -9,12 +20,43 @@ describe('Welcome Page User Flow', () => {
     cy.get('.title-r').should('contain', 'r')
   })
 
-  // it('Should see welcome message', () => {
-  //   cy.get('.welcome-message').should('contain', 'FIND YOUR PERFECT OFF-ROAD TRAIL')
-  // })
+  it('Should see personalized welcome message to user', () => {
+    cy.get('.welcome-user').should('contain', 'Welcome, Shawn Lee')
+  })
 
-  // it('Should see a button to find trails', () => {
-  //   cy.get('.button-welcome').should('contain', 'FIND YOUR TRAIL ▶︎')
-  // })
+  it('Should see favorites button, about button and sign out button', () => {
+    cy.get('.saved-page-button').should('contain', 'Favorites')
+    cy.get('.sign-out').should('contain', 'Sign Out')
+    cy.get('.about-button').should('contain', 'About')
+  })
+
+  it('Should see welcome message', () => {
+    cy.get('.welcome-message').should('contain', 'FIND YOUR PERFECT OFF-ROAD TRAIL')
+  })
+
+  it('Should see a button to find trails', () => {
+    cy.get('.button-welcome').should('contain', 'FIND YOUR TRAIL ▶︎')
+  })
+
+  it('Should be able to navigate to favorites page and back', () => {
+    cy.get('.saved-page-button').click()
+    cy.url().should('eq', 'http://localhost:3001/saved_trails')
+    cy.get('.title').click()
+    cy.url().should('eq', 'http://localhost:3001/home')
+  })
+
+  it('Should be able to navigate to about page and back', () => {
+    cy.get('.about-button').click()
+    cy.url().should('eq', 'http://localhost:3001/about')
+    cy.get('.title').click()
+    cy.url().should('eq', 'http://localhost:3001/home')
+  })
+
+  it('Should be able to navigate to all trails page and back', () => {
+    cy.get('.button-welcome').click()
+    cy.url().should('eq', 'http://localhost:3001/trails')
+    cy.get('.title').click()
+    cy.url().should('eq', 'http://localhost:3001/home')
+  })
 
 })
