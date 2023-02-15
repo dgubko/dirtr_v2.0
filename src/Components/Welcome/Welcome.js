0d72b28355ba696270d29dc110b963ccb8e9ece7
Dana Truong <tramtramphotos@gmail.com>
2023-02-15 10:57:15 -0600

    Change Offroad to Off-Road in welcome message

--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -5,7 +5,7 @@ import { NavLink } from 'react-router-dom'
 const Welcome = () => {
   return (
     <div className="welcome-main">
-      <h1 className="welcome-message">FIND YOUR PERFECT OFFROAD TRAIL</h1>
+      <h1 className="welcome-message">FIND YOUR PERFECT OFF-ROAD TRAIL</h1>
         <NavLink to="/trails">
           <button className="button-welcome">
             FIND YOUR TRAIL ▶︎
