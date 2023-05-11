741bf7a1195e5a440ad93e24cb4cd22895e2d177
Dmitrii Gubko <dalnoboy29region@gmail.com>
2023-05-10 21:41:48 -0600

    Fix issue with changing functionality

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,12 +4,13 @@ import { logOut, setUser } from "../../store/actions";
 import { useSelector, useDispatch } from "react-redux";
 
 const Header = () => {
-  const isLogged = useSelector((state) => state.isLogged);
-  const selectedUser = useSelector((state) => state.selectedUser.data);
+  const { isLogged, user } = useSelector((state) => ({
+    isLogged: state.session.isLogged,
+    user: state.selectedUser,
+  }));
   const dispatch = useDispatch();
   const signOut = () => {
     dispatch(logOut());
-    dispatch(setUser({ data: { attributes: { name: "signed-out" } } }));
   };
 
   const checkIsActive = ({ isActive }) =>
@@ -35,10 +36,10 @@ const Header = () => {
           About Us
         </NavLink>
       </nav>
-      {isLogged ? (
+      {isLogged && user ? (
         <div className={styles.account}>
           <p>
-            Welcome, <strong>{selectedUser.attributes.name}</strong>
+            Welcome, <strong>{user.name}</strong>
           </p>
           <NavLink to="/">
             <button className="sign-out" onClick={() => signOut()}>
